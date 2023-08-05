ae24623d8a6470400433f7441fe67fe1b3f2fe29
Azusa <sora65536@outlook.com>
2023-08-06 00:46:05 +0800

    修复netty客户端

--- netty/src/main/java/Asuza/netty/client/NettyWebCrawler.java
+++ netty/src/main/java/Asuza/netty/client/NettyWebCrawler.java
@@ -1,17 +1,13 @@
 package Asuza.netty.client;
 
 import io.netty.bootstrap.Bootstrap;
-import io.netty.buffer.ByteBuf;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.http.*;
-import io.netty.util.CharsetUtil;
 import io.netty.util.concurrent.Future;
-import io.netty.util.concurrent.FutureListener;
 
 import java.net.URI;
-import java.net.InetSocketAddress;
 import java.net.URISyntaxException;
 
 public class NettyWebCrawler {
@@ -26,41 +22,34 @@ public class NettyWebCrawler {
                 .channel(NioSocketChannel.class)
                 .handler(new ChannelInitializer<Channel>() {
                     @Override
-                    protected void initChannel(Channel ch) throws Exception {
+                    protected void initChannel(Channel ch){
                         ch.pipeline().addLast(new HttpClientCodec());
                         ch.pipeline().addLast(new HttpContentDecompressor());
-                        ch.pipeline().addLast(new HttpObjectAggregator(99999));
-                        ch.pipeline().addLast(new SimpleChannelInboundHandler<HttpObject>() {
+                        ch.pipeline().addLast(new HttpObjectAggregator(Integer.MAX_VALUE));
+                        ch.pipeline().addLast(new SimpleChannelInboundHandler<FullHttpResponse>() {
                             @Override
-                            public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                            public void channelActive(ChannelHandlerContext ctx) {
                                 // 要发送的 GET 请求的 URL
-
-//                                FullHttpRequest request = new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, uri.toASCIIString());
-//                                request.headers().add(HttpHeaderNames.CONNECTION,HttpHeaderValues.KEEP_ALIVE);
-//                                request.headers().add(HttpHeaderNames.CONTENT_LENGTH,request.content().readableBytes());
                                 HttpRequest request = new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, uri.getRawPath());
                                 HttpHeaders headers = request.headers();
-                                headers.set(HttpHeaders.Names.HOST, uri.getHost());
-                                headers.set(HttpHeaders.Names.CONNECTION, HttpHeaders.Values.CLOSE);
-                                headers.set(HttpHeaders.Names.USER_AGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.0.0 Safari/537.36"); // 设置浏览器 User-Agent
-                                System.out.println("发送了吗");
+                                headers.add(HttpHeaderNames.HOST, uri.getHost());
+                                headers.add(HttpHeaderNames.CONNECTION, HttpHeaderValues.CLOSE);
+                                // 设置浏览器 User-Agent
+                                headers.add(HttpHeaderNames.USER_AGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.0.0 Safari/537.36");
                                 // 发送 GET 请求
                                 ctx.writeAndFlush(request);
                             }
                             @Override
-                            protected void channelRead0(ChannelHandlerContext channelHandlerContext, HttpObject msg) throws Exception {
-                                System.out.println("有数据吗");
+                            protected void channelRead0(ChannelHandlerContext channelHandlerContext, FullHttpResponse msg) {
+                                // 响应头
                                 System.out.println(msg.toString());
-                                if (msg instanceof HttpContent) {
-                                    HttpContent content = (HttpContent) msg;
-                                    int length = content.content().readableBytes();
-                                    byte[] bytes = new byte[length];
-                                    content.content().readBytes(bytes);
-                                    System.out.println(new String(bytes));
-                                }
+                                // 响应体
+                                int length = msg.content().readableBytes();
+                                byte[] bytes = new byte[length];
+                                msg.content().readBytes(bytes);
+                                System.out.println(new String(bytes));
                             }
                         });
-                        // Add other handlers as needed
                     }
                 });
 
@@ -73,13 +62,5 @@ public class NettyWebCrawler {
         } finally {
             group.shutdownGracefully();
         }
-    }
-
-    private static void sendGetRequest(Bootstrap bootstrap, String url) throws Exception {
-        URI uri = new URI(url);
-
-        ChannelFuture channelFuture = bootstrap.connect(uri.getHost(), 80).sync();
-        channelFuture.channel().closeFuture().sync();
-
     }
 }
