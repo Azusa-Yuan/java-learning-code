466e6cd17c15c3acb4c897fbe557fef9dd93b98b
Azusa <sora65536@outlook.com>
2023-09-08 21:28:30 +0800

    补充jvm，类加载器还没有整理完

---
+++ jvm/src/main/java/Asuza/classLoader/类加载器.java
@@ -0,0 +1,14 @@
+package Asuza.classLoader;
+
+/**
+ * Created by 周杰伦 on 2018/6/8.
+ */
+public class 类加载器 {
+    public static void main(String[] args) {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        System.out.println(Thread.currentThread().getContextClassLoader());
+        System.out.println(classLoader.getParent());
+        System.out.println(classLoader.getParent().getParent());
+
+    }
+}
